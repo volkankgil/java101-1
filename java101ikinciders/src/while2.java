e940c661ffeebcc8be912d6499c559e739a24e7e
volkankgil <104678474+volkankgil@users.noreply.github.com>
2022-05-10 16:02:08 +0300

    yeni dosyalar

---
+++ java101ikinciders/src/while2.java
@@ -0,0 +1,14 @@
+
+
+package döngüler;
+
+public class while2 {
+    public static void main(String[] args) {
+        int left = 100, right = 200;
+
+        while (++left < --right) ;
+        System.out.println(left);
+    }
+}
+
+
