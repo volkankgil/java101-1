3d894057f93da81e5ad3c720a0f999aea5edecea
volkankgil <104678474+volkankgil@users.noreply.github.com>
2022-05-29 23:21:37 +0300

    ebob ekok

---
+++ java101ikinciders/src/döngüler/harmonikseri.java
@@ -0,0 +1,20 @@
+package java101ikinciders.src.döngüler;
+
+import java.util.Scanner;
+
+public class harmonikseri {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("N sayýsýný giriniz: ");
+        int n=input.nextInt();
+
+        double sum=0;
+
+        for(double i=1;i<=n;i++){
+            sum+=(1/i);
+        }
+        System.out.println(sum);
+
+    }
+}
