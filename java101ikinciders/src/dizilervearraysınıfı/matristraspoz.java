172ff55365e545eab18ce3aeb85a42b8169973f4
volkankgil <volkankgil@gmail.com>
2022-06-17 13:13:53 +0300

    ebob ekok

--- java101ikinciders/src/dizilervearraysınıfı/matristraspoz.java
+++ java101ikinciders/src/dizilervearraysınıfı/matristraspoz.java
@@ -3,31 +3,48 @@ package java101ikinciders.src.dizilervearraysınıfı;
 import java.util.Arrays;
 
 public class matristraspoz {
+
+    static void transpoz(int[][] arr) {
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
     public static void main(String[] args) {
 
-        int[][] list = {{1, 2, 3,4,5},
-                {6, 7, 8,9,10},
-                {10, 11,12,13,14}
+
+        int[][] list = {{1, 2, 3, 4, 5, 6},
+                {6, 7, 8, 9, 10, 12},
+                {10, 11, 12, 13, 14, 15}
 
         };
 
         for (int i = 0; i < list.length; i++) {
             for (int j = 0; j < list[0].length; j++) {
-                System.out.print(list[i][j]+" ");
+                System.out.print(list[i][j] + " ");
 
             }
             System.out.println(" ");
 
         }
+
+        int newrow = list[0].length;
+        int newcol = list.length;
 
         System.out.println("----------------");
 
-        for (int i = 0; i < list.length; i++) {
-            for (int j = 0; j < list[0].length; j++) {
-                System.out.print(list[i][j]+" ");
+        for (int i = 0; i < newrow; i++) {
+            for (int j = 0; j < newcol; j++) {
+                System.out.print(list[j][i] + " ");
             }
             System.out.println(" ");
 
         }
+
+        System.out.println("*************");
+
+        transpoz(list);
     }
 }
